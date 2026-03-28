b0e652fc20c2ead64fbf63c0135c317b32afe316
Nikita <nikita.polischukov@gmail.com>
2026-03-29 00:22:45 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+def test_buy_more_cryptocurrency() -> None:
+    with (
+        patch("app.main.get_exchange_rate_prediction", return_value=106)
+    ):
+        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_sell_all_your_cryptocurrency() -> None:
+    with (
+        patch("app.main.get_exchange_rate_prediction", return_value=94)
+    ):
+        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+def test_do_nothing_with_cryptocurrency() -> None:
+    with (
+        patch("app.main.get_exchange_rate_prediction", return_value=100)
+    ):
+        assert cryptocurrency_action(100) == "Do nothing"
